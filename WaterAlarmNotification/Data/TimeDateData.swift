ab863eb9e71f4a03b1423a018a2466951faa0837
HyeongHwan <gudghks56@gmail.com>
2022-03-19 23:45:54 +0900

    Date Struct

---
+++ WaterAlarmNotification/Data/TimeDateData.swift
@@ -0,0 +1,16 @@
+//
+//  TimeDateData.swift
+//  WaterAlarmNotification
+//
+//  Created by 박형환 on 2022/03/14.
+//
+
+import Foundation
+
+struct TimeDateData{
+    var uuid: String
+    var year_Month_Day: String
+    var time: String
+    var amORpm: String
+    var isSelected: Bool
+}
